1328ec7f28fa1e8d6711fdbcf14eb741f09779d9
chaalia <rejeb.chaalia@gmail.com>
2021-06-04 00:04:13 +0100

    add service model

--- pynote/customers/models.py
+++ pynote/customers/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+
+class Service(models.Model):
+    name = models.CharField("Name", max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Customer(models.Model):
     first_name = models.CharField("First name", max_length=255)
     last_name = models.CharField("Last name", max_length=255)
@@ -8,6 +16,7 @@ class Customer(models.Model):
     address =  models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     createdAt = models.DateTimeField("Created At", auto_now_add=True)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.first_name
